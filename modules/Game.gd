13d6b933a323069757d376a46a4c0a5465be1c8c
nelso <nelsontron@outlook.com>
2024-06-17 18:14:09 -0600

    removed old stuff

--- modules/Game.gd
+++ modules/Game.gd
@@ -1,5 +1,13 @@
 extends Node
 
+func _ready() -> void:
+	var xr_interface = XRServer.find_interface("OpenXR")
+	if xr_interface and xr_interface.is_initialized():
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+		get_viewport().use_xr = true
+	else:
+		print("No VR interface found.")
+
 func get_game_node(path: NodePath): return get_tree().root.get_node("Node3D").get_node(path)
 func get_player(): return get_game_node("Player") as Player
 func get_player_node(path: NodePath): return get_player().get_node(path)
