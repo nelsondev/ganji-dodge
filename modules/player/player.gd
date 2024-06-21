6905d512d496379c189ac78527d10ef55a89a17f
nelso <nelsontron@outlook.com>
2024-06-20 18:45:06 -0600

    movement kinda

--- modules/player/player.gd
+++ modules/player/player.gd
@@ -5,10 +5,14 @@ const SPEED = 4.5
 
 func _physics_process(delta: float) -> void:
 	var input_dir = Input.get_vector("player_left", "player_right", "player_up", "player_down")
-	var direction = ($PivotCamera.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if input_dir.is_zero_approx():
+		input_dir = $XROrigin3D/XRLeftHand.get_vector2("primary")
+		input_dir.y *= -1
+	
+	var direction = ($XROrigin3D/XRCamera.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
 	direction.y = 0
-
+	
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
