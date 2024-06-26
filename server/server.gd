ebccc0ada0aee9b52ddc713a4cf620f3321bcdb8
Nelson M <nelsontron@outlook.com>
2024-06-25 18:41:00 -0700

    landscape and fences

--- server/server.gd
+++ server/server.gd
@@ -3,6 +3,7 @@ extends Node
 var _port = 5000
 var _max_players = 16
 var _peer = ENetMultiplayerPeer.new()
+var started = Time.get_unix_time_from_system()
 
 var _tickrate = 30
 var _tick_timer = Timer.new()
@@ -10,18 +11,24 @@ var _tick_timer = Timer.new()
 var _players: Dictionary = {}
 
 func _ready():
+	print("Starting server...")
+	print("\tPORT: ", _port)
+	print("\tMAX_PLAYERS: ", _max_players)
+	print("\tTICK_RATE: ", _tickrate)
+	
 	_peer.create_server(_port, _max_players)
 	
 	multiplayer.multiplayer_peer = _peer
 	multiplayer.peer_connected.connect(_peer_connected)
 	multiplayer.peer_disconnected.connect(_peer_disconnected)
-
 	# Setup tick function
 	_tick_timer.one_shot = false
 	_tick_timer.wait_time = 1.0 / _tickrate
 	_tick_timer.timeout.connect(_tick)
 	add_child(_tick_timer)
 	_tick_timer.start()
+	
+	print("Server ready for connection in ", Time.get_unix_time_from_system() - started, " ms.")
 
 func _tick():
 	Client._sync.rpc(_players)
